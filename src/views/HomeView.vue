3a2349733639eb5ffafde5ed8d91174157d3a72a
David Barrero <davbarfer@gmail.com>
2024-04-04 18:02:57 +0200

    Add component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@ import { links, donate } from '@/components/data/links'
 import ProfilePicture from '@/components/ProfilePicture.vue'
 import LinkBtn from '@/components/LinkBtn.vue'
 import Separator from '@/components/SeparatorLine.vue'
+import FooterIcons from '@/components/FooterIcons.vue'
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-y-8">
     <ProfilePicture class="mt-8" />
     <h1 class="font-bold text-xl text-center uppercase">david barrero</h1>
+    <FooterIcons />
     <Separator />
     <LinkBtn
       v-for="item in links"
