cfa25419664e11264e572b5aecc95da7d58d099e
David Barrero <davbarfer@gmail.com>
2024-04-04 15:11:58 +0200

    Add links, donate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
+import { links, donate } from '@/components/data/links'
 import ProfilePicture from '@/components/ProfilePicture.vue'
 import LinkBtn from '@/components/LinkBtn.vue'
-import { links } from '@/components/data/links'
 import Separator from '@/components/SeparatorLine.vue'
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-y-8">
-    <ProfilePicture />
-    <Separator></Separator>
+    <ProfilePicture class="mt-8" />
+    <h1 class="font-bold text-xl text-center uppercase">david barrero</h1>
+    <Separator />
     <LinkBtn
       v-for="item in links"
       :key="item.title"
       :title="item.title"
       :link="item.link"
       :alt="item.alt"
+    ></LinkBtn>
+    <Separator />
+    <LinkBtn
+      :title="donate.title"
+      :link="donate.link"
+      :alt="donate.alt"
     ></LinkBtn>
   </div>
 </template>
