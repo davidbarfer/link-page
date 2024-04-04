fe0c18bf3159c828fcb13ad79c596dd8ea0184f8
David Barrero <davbarfer@gmail.com>
2024-04-04 13:18:06 +0200

    Add separator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import ProfilePicture from '@/components/ProfilePicture.vue'
 import LinkBtn from '@/components/LinkBtn.vue'
 import { links } from '@/components/data/links'
+import Separator from '@/components/SeparatorLine.vue'
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-y-8">
     <ProfilePicture />
+    <Separator></Separator>
     <LinkBtn
       v-for="item in links"
       :key="item.title"
