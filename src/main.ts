cea9f8123d2b88219bb4e09fd85f67d219c1525d
David Barrero <davbarfer@gmail.com>
2024-04-04 18:28:12 +0200

    Delete analytics config

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import App from './App.vue'
 import './style.css'
 import router from './router'
 
-// Vercel analytics
-import { inject } from '@vercel/analytics'
-
-inject()
-
 const app = createApp(App)
 
 app.use(router)
