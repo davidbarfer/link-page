7725427ded71627ec3aa37b0dd1cdf0499152ab9
David Barrero <davbarfer@gmail.com>
2024-04-04 15:11:33 +0200

    Update link data

--- src/components/data/links.ts
+++ src/components/data/links.ts
@@ -3,15 +3,42 @@ interface link {
   link: string
   alt?: string
 }
-export const links: link[] = [
+export const socialMedia: link[] = [
   {
-    title: 'instagram',
-    link: 'https://www.instagram.com/david_byhunter/',
-    alt: 'Instagram Profile'
+    title: 'linkedin',
+    link: 'https://www.linkedin.com/in/davidbarfer/',
+    alt: 'LinkedIn Profile'
   },
   {
     title: 'github',
     link: 'https://github.com/David-ByHunter',
     alt: 'GitHub Profile'
+  },
+  {
+    title: 'instagram',
+    link: 'https://www.instagram.com/david_byhunter/',
+    alt: 'Instagram Profile'
   }
 ]
+export const links: link[] = [
+  {
+    title: 'ESTIEM LG Seville',
+    link: 'http://estiemlgseville.org/',
+    alt: 'ESTIEM LG Seville Website'
+  },
+  {
+    title: 'ESTIEM',
+    link: 'http://estiem.org/',
+    alt: 'ESTIEM Website'
+  },
+  {
+    title: 'Buhurt International',
+    link: 'https://www.buhurtinternational.com/',
+    alt: 'BI Website'
+  }
+]
+export const donate: link = {
+  title: 'Donate',
+  link: 'https://www.paypal.com/paypalme/davidbyhunter',
+  alt: 'Paypal Me Donate'
+}
