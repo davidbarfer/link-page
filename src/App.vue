764635e72fe979f97f7e15831a38d7208e396bc0
David Barrero <davbarfer@gmail.com>
2024-11-26 17:09:09 +0100

    Use Vue import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { Analytics } from '@vercel/analytics/nuxt'
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
